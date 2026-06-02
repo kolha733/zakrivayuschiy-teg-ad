00fa98e428d3a91fdc82b42c781fc5614de00961
kolha733 <42875264+kolha733@users.noreply.github.com>
2026-06-02 05:15:41 +0300

    Ver1.1

--- scripts/dialog.js
+++ scripts/dialog.js
@@ -1,10 +1,17 @@
 const saveButton = document.querySelector('.save-button');
 const memoryDialog = document.querySelector('#memory-dialog');
+const dialogButton = document.querySelector('.dialog__button');
 
 if (saveButton && memoryDialog) {
   saveButton.addEventListener('click', () => {
     if (!memoryDialog.open) {
       memoryDialog.showModal();
     }
+  });
+}
+
+if (dialogButton && memoryDialog) {
+  dialogButton.addEventListener('click', () => {
+    memoryDialog.close();
   });
 }
